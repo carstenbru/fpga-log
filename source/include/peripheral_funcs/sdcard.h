2c84b57408f3ce70d7f41e9fbfc0ff3a52366e41
Carsten Bruns <bruns@lichttechnik.tu-darmstadt.de>
2014-05-22 22:03:54 +0200

    sdcard peripheral c functions

---
+++ source/include/peripheral_funcs/sdcard.h
@@ -0,0 +1,41 @@
+/**
+ * @file sdcard.h
+ * @brief sdcard peripheral functions
+ *
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+#ifndef SDCARD_H_
+#define SDCARD_H_
+
+#include <peripherals/sdcard.h>
+
+/**
+ * initializes a connected sdcard
+ *
+ * @param sdcard	pointer to the sdcard peripheral
+ * @return INIT_NO_ERROR on success, otherwise error code
+ */
+init_code sdcard_init(sdcard_regs_t* const sdcard);
+
+/**
+ * writes a block (512 bytes) to the sdcard
+ *
+ * @param sdcard	pointer to the sdcard peripheral
+ * @param address	destination address (on sdcard) of the block write
+ * @param block		pointer to the data block in memory
+ * @return	WRITE_NO_ERROR on success, otherwise error code
+ */
+write_code sdcard_block_write(sdcard_regs_t* const sdcard, unsigned long address, unsigned char* block);
+
+/**
+ * reads a block (512 bytes) from the sdcard
+ *
+ * @param sdcard	pointer to the sdcard peripheral
+ * @param address	source address (on sdcard) of the block read
+ * @param block		pointer to the data destination in memory
+ * @return	READ_NO_ERROR on success, otherwise error code
+ */
+read_code sdcard_block_read(sdcard_regs_t* const sdcard, unsigned long address, unsigned char* block);
+
+#endif
---
+++ source/src/sdcard.c
@@ -0,0 +1,81 @@
+/**
+ * @file sdcard.c
+ * @brief sdcard peripheral functions
+ *
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+#include "peripheral_funcs/sdcard.h"
+
+/**
+ * waits for the sdcard peripheral to be ready
+ *
+ * @param sdcard	pointer to the sdcard peripheral
+ */
+static sdcard_busy_wait(sdcard_regs_t* const sdcard) {
+	while (sdcard->trans_sts)
+		;
+}
+
+init_code sdcard_init(sdcard_regs_t* const sdcard) {
+	sdcard->trans_type = TRANS_TYPE_INIT_SD;
+	sdcard->trans_ctrl = TRANS_START;
+
+	sdcard_busy_wait(sdcard);
+
+	return sdcard->trans_error & 3;
+}
+
+/**
+ * sets the address register in a sdcard peripheral
+ *
+ * @param sdcard	pointer to the sdcard peripheral
+ * @param address	new address value
+ */
+static void sdcard_set_address(sdcard_regs_t* const sdcard,
+		unsigned long int address) {
+	sdcard->sd_addr_7_0 = address;
+	sdcard->sd_addr_15_8 = address >> 8;
+	sdcard->sd_addr_23_16 = address >> 16;
+	sdcard->sd_addr_31_24 = address >> 24;
+}
+
+write_code sdcard_block_write(sdcard_regs_t* const sdcard,
+		unsigned long address, unsigned char* block) {
+	int i;
+	for (i = 0; i < SD_BLOCK_SIZE; i++) {  //write data to peripheral fifo
+		sdcard->tx_fifo_data = *block++;
+	}
+	sdcard_set_address(sdcard, address);
+	sdcard->trans_type = TRANS_TYPE_RW_WRITE_SD_BLOCK;
+	sdcard->trans_ctrl = TRANS_START;
+
+	sdcard_busy_wait(sdcard);
+	return sdcard->trans_error >> 4;
+}
+
+read_code sdcard_block_read(sdcard_regs_t* const sdcard, unsigned long address,
+		unsigned char* block) {
+	sdcard_set_address(sdcard, address);
+	sdcard->trans_type = TRANS_TYPE_RW_READ_SD_BLOCK;
+	sdcard->trans_ctrl = TRANS_START;
+
+	sdcard_busy_wait(sdcard);
+	if ((sdcard->trans_error >> 2) != READ_NO_ERROR)
+		return (sdcard->trans_error >> 2);
+
+	/*
+	 * dummy read:
+	 * Since sdcard peripheral was originally designed for a wishbone bus with supports delays by the peripheral
+	 * (and SpartanMCs bus does not), the fifo has a delay of one clock cycle.
+	 * So the read data is always the last value and not the real one.
+	 */
+	*block = sdcard->rx_fifo_data;
+	int i;
+	for (i = 0; i < SD_BLOCK_SIZE; i++) {  //read data from peripheral fifo
+		*block++ = sdcard->rx_fifo_data;
+	}
+	sdcard->rx_fifo_control = FIFO_FORCE_EMPTY; //clear last pending read in peripheral, refers to the dummy read
+
+	return READ_NO_ERROR;
+}
