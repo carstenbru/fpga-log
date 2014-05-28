b0209e77c85e705ca4de5ad36e2ff312e5ce1e64
Carsten Bruns <bruns@lichttechnik.tu-darmstadt.de>
2014-05-28 14:44:21 +0200

    control actions

---
+++ source/include/dm/control_action.h
@@ -0,0 +1,38 @@
+/**
+ * @file control_action.h
+ * @brief control actions
+ *
+ * This can be used for example as an action performed on a trigger or timer event.
+ * Use this as super-"class" for more complex conditions.
+ *
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+#ifndef CONTROL_ACTION_H_
+#define CONTROL_ACTION_H_
+
+#include "control_port.h"
+
+/**
+ * @brief struct defining a control action
+ *
+ * Use this as super-"class" for more complex conditions.
+ */
+typedef struct {
+	/**
+	 * @brief executes the control action
+	 *
+	 * @param control_action	pointer to this struct
+	 * @param port						pointer to the control port to which the command should be sent
+	 */
+	void (*execute)(void* const control_action, const control_port_t* port);
+} control_action_t;
+
+/**
+ * @brief measure control action
+ *
+ * This action can be used to start a measurement.
+ */
+extern control_action_t control_action_measure;
+
+#endif
---
+++ source/src/control_action.c
@@ -0,0 +1,19 @@
+/**
+ * @file control_action.h
+ * @brief control actions
+ *
+ * This can be used for example as an action performed on a trigger or timer event.
+ * Use this as super-"class" for more complex conditions.
+ *
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+#include "dm/control_action.h"
+
+static void control_action_measure_func(void* const control_action,
+		const control_port_t* port) {
+	control_parameter_t p = { 'm', 0 };
+	port->new_control_message(port->parent, 1, &p);
+}
+
+control_action_t control_action_measure = { control_action_measure_func };
