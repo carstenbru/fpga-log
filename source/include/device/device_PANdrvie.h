bd45dade4bd713a6c77ce86e249f13696a6fb358
Carsten Bruns <bruns@lichttechnik.tu-darmstadt.de>
2014-11-21 15:05:45 +0100

    PANdrive device driver

---
+++ source/include/device/device_PANdrvie.h
@@ -0,0 +1,186 @@
+/**
+ * @file device_PANdrive.h
+ * @brief device driver (UART) for PANdrive motors of trinamic
+ *
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+#ifndef DEVICE_PANDRIVE_H_
+#define DEVICE_PANDRIVE_H_
+
+#include <uart.h>
+
+#include <fpga-log/datastream_source.h>
+#include <fpga-log/data_port.h>
+#include <fpga-log/control_port.h>
+
+#define PAN_DRIVE_DEFAULT_TARGET_ADDRESS 0x01
+
+#define PAN_DRIVE_ERROR_CODES { "wrong checksum", "invalid command", "wrong type", "invalid value", "EEPROM locked", "not available" }
+
+typedef enum {
+	PAN_DRIVE_MODULE_ADDRESS = 'a',
+	PAN_DRIVE_COMMAND = 'c',
+	PAN_DRIVE_TYPE = 't',
+	PAN_DRIVE_MOTOR_NUMBER = 'm',
+	PAN_DRIVE_BANK_NUMBER = 'b',
+	PAN_DRIVE_VALUE = 'v',
+} pan_drive_command_cpt;
+
+typedef enum {
+	PAN_DRIVE_COMMAND_ROTATE_RIGHT = 1,
+	PAN_DRIVE_COMMAND_ROTATE_LEFT = 2,
+	PAN_DRIVE_COMMAND_MOTOR_STOP = 3,
+	PAN_DRIVE_COMMAND_MOVE_TO_POSTION = 4,
+
+	PAN_DRIVE_COMMAND_SET_AXIS_PARAMETER = 5,
+	PAN_DRIVE_COMMAND_GET_AXIS_PARAMETER = 6,
+	PAN_DRIVE_COMMAND_STORE_AXIS_PARAMTER = 7,
+	PAN_DRIVE_COMMAND_RESTORE_AXIS_PARAMTER = 8,
+
+	PAN_DRIVE_COMMAND_SET_GLOBAL_PARAMETER = 9,
+	PAN_DRIVE_COMMAND_GET_GLOBAL_PARAMETER = 10,
+	PAN_DRIVE_COMMAND_STORE_GLOBAL_PARAMTER = 11,
+	PAN_DRIVE_COMMAND_RESTORE_GLOBAL_PARAMTER = 12,
+
+	PAN_DRIVE_COMMAND_REFERENCE_SEARCH = 13,
+	PAN_DRIVE_COMMAND_SET_OUTPUT = 14,
+	PAN_DRIVE_COMMAND_GET_IN_OUTPUT = 15,
+
+	/* gap: TMCL only commands */
+
+	PAN_DRIVE_COMMAND_SET_COORDINATE = 30,
+	PAN_DRIVE_COMMAND_GET_COORDINATE = 31,
+	PAN_DRIVE_COMMAND_CAPTURE_COORDINATE = 32,
+
+	/* TODO implement accumulator commands? not really necessary... */
+
+	PAN_DRIVE_COMMAND_TARGET_REACHED_EVENT = 138,
+} pan_drive_command_code_cpt;
+
+typedef enum {
+	PAN_DRIVE_TYPE_DONT_CARE = 0,
+
+	/* types for PAN_DRIVE_COMMAND_MOVE_TO_POSTION */
+	PAN_DRIVE_TYPE_POS_ABS = 0,
+	PAN_DRIVE_TYPE_POS_REL = 1,
+	PAN_DRIVE_TYPE_POS_COORD = 2,
+
+	PAN_DRIVE_TYPE_APARAM_TARGET_POS = 0,
+	PAN_DRIVE_TYPE_APARAM_ACT_POS = 1,
+	PAN_DRIVE_TYPE_APARAM_TARGET_SPEED = 2,
+	PAN_DRIVE_TYPE_APARAM_ACT_SPEED = 3,
+	PAN_DRIVE_TYPE_APARAM_MAX_POS_SPEED = 4,
+	PAN_DRIVE_TYPE_APARAM_MAX_ACC = 5,
+	PAN_DRIVE_TYPE_APARAM_MAX_CUR = 6,
+	PAN_DRIVE_TYPE_APARAM_STDBY = 7,
+	PAN_DRIVE_TYPE_APARAM_TARGET_REACHED = 8,
+	PAN_DRIVE_TYPE_APARAM_REF_SW = 9,
+	PAN_DRIVE_TYPE_APARAM_RIGHT_SW = 10,
+	PAN_DRIVE_TYPE_APARAM_LEFT_SW = 11,
+	PAN_DRIVE_TYPE_APARAM_RIGHT_SW_DISABLE = 12,
+	PAN_DRIVE_TYPE_APARAM_LEFT_SW_DISABLE = 13,
+	PAN_DRIVE_TYPE_APARAM_MIN_SPEED = 130,
+	PAN_DRIVE_TYPE_APARAM_ACT_ACC = 135,
+	PAN_DRIVE_TYPE_APARAM_RAMP_MODE = 138,
+	PAN_DRIVE_TYPE_APARAM_MICROSTEP_RES = 140,
+	PAN_DRIVE_TYPE_APARAM_SOFT_STOP = 149,
+	PAN_DRIVE_TYPE_APARAM_RAMP_DIV = 153,
+	PAN_DRIVE_TYPE_APARAM_PULSE_DIV = 154,
+	PAN_DRIVE_TYPE_APARAM_REF_MODE = 193,
+	PAN_DRIVE_TYPE_APARAM_REF_SEARCH_SPEED = 194,
+	PAN_DRIVE_TYPE_APARAM_REF_SW_SPEED = 195,
+	PAN_DRIVE_TYPE_APARAM_DIST_SW = 196,
+	PAN_DRIVE_TYPE_APARAM_MIXED_DECAY = 203,
+	PAN_DRIVE_TYPE_APARAM_FREEWHEELING = 204,
+	PAN_DRIVE_TYPE_APARAM_STALL_THRESHOLD = 205,
+	PAN_DRIVE_TYPE_APARAM_ACT_LOAD = 206,
+	PAN_DRIVE_TYPE_APARAM_DRIVER_ERROR = 208,
+	PAN_DRIVE_TYPE_APARAM_ENC_POS = 210,
+	PAN_DRIVE_TYPE_APARAM_FULLSTEP_THRESHOLD = 211,
+	PAN_DRIVE_TYPE_APARAM_MAX_ENC_DERIVATION = 212,
+	PAN_DRIVE_TYPE_APARAM_POWER_DOWN_DELAY = 214,
+	PAN_DRIVE_TYPE_APARAM_ABS_ENC_VAL = 215,
+	/* TODO Global parameters (chapter 7) not so important.. */
+
+	/* types for PAN_DRIVE_COMMAND_REFERENCE_SEARCH */
+	PAN_DRIVE_TYPE_REF_START = 0,
+	PAN_DRIVE_TYPE_REF_STOP = 1,
+	PAN_DRIVE_TYPE_REF_STATUS = 2,
+
+	/* types for PAN_DRIVE_COMMAND_SET_OUTPUT / PAN_DRIVE_COMMAND_GET_IN_OUTPUT */
+	PAN_DRIVE_TYPE_PIN_0 = 0,
+	PAN_DRIVE_TYPE_PIN_1 = 1,
+
+	/* types for PAN_DRIVE_COMMAND_SET_COORDINATE / PAN_DRIVE_COMMAND_GET_COORDINATE / PAN_DRIVE_COMMAND_CAPTURE_COORDINATE */
+	PAN_DRIVE_TYPE_COORDINATE_0 = 0,
+	PAN_DRIVE_TYPE_COORDINATE_1 = 1,
+	PAN_DRIVE_TYPE_COORDINATE_2 = 2,
+	PAN_DRIVE_TYPE_COORDINATE_3 = 3,
+	PAN_DRIVE_TYPE_COORDINATE_4 = 4,
+	PAN_DRIVE_TYPE_COORDINATE_5 = 5,
+	PAN_DRIVE_TYPE_COORDINATE_6 = 6,
+	PAN_DRIVE_TYPE_COORDINATE_7 = 7,
+	PAN_DRIVE_TYPE_COORDINATE_8 = 8,
+	PAN_DRIVE_TYPE_COORDINATE_9 = 9,
+	PAN_DRIVE_TYPE_COORDINATE_10 = 10,
+	PAN_DRIVE_TYPE_COORDINATE_11 = 11,
+	PAN_DRIVE_TYPE_COORDINATE_12 = 12,
+	PAN_DRIVE_TYPE_COORDINATE_13 = 13,
+	PAN_DRIVE_TYPE_COORDINATE_14 = 14,
+	PAN_DRIVE_TYPE_COORDINATE_15 = 15,
+	PAN_DRIVE_TYPE_COORDINATE_16 = 16,
+	PAN_DRIVE_TYPE_COORDINATE_17 = 17,
+	PAN_DRIVE_TYPE_COORDINATE_18 = 18,
+	PAN_DRIVE_TYPE_COORDINATE_19 = 19,
+	PAN_DRIVE_TYPE_COORDINATE_20 = 20,
+} pan_drive_command_type;
+
+/**
+ * @brief struct describing a PANdrive device
+ */
+typedef struct {
+	datastream_source_t super; /**< super-"class": datastream_source_t*/
+
+	const data_port_t* data_out; /**< data output destination */
+	const data_port_t* status_out; /**< status output destination */
+	control_port_t control_in; /**< control port, this can be set at a control output to direct the control stream to this device */
+
+	uart_light_regs_t* uart_light; /**< pointer to UART hardware registers */
+
+	unsigned char target_address;
+	int receive_state;
+	unsigned char received_status;
+	int reply_command_code;
+	long received_value;
+
+	unsigned char* buf;
+} device_PANdrive_t;
+
+void device_PANdrive_init(device_PANdrive_t* const pan_drive,
+		uart_light_regs_t* const uart_light, const int id);
+
+control_port_t* device_PANdrive_get_control_in(
+		device_PANdrive_t* const pan_drive);
+
+void device_PANdrive_set_data_out(device_PANdrive_t* const pan_drive,
+		const data_port_t* const data_in);
+
+void device_PANdrive_set_status_out(device_PANdrive_t* const pan_drive,
+		const data_port_t* const data_in);
+
+void device_PANdrive_send_command(device_PANdrive_t* const pan_drive,
+		pan_drive_command_code_cpt command_code, pan_drive_command_type type,
+		long value);
+
+void device_PANdrive_send_command_w_mbnum(device_PANdrive_t* const pan_drive,
+		pan_drive_command_code_cpt command_code, unsigned char type,
+		unsigned char mbnum, long value);
+
+void device_PANdrive_set_target_address(device_PANdrive_t* const pan_drive,
+		unsigned char new_address);
+
+void device_PANdrive_set_axis_parameter(device_PANdrive_t* const pan_drive,
+		pan_drive_command_type type, long value);
+
+#endif
---
+++ source/src/device_PANdrvie.c
@@ -0,0 +1,205 @@
+/**
+ * @file device_PANdrive.c
+ * @brief device driver (UART) for PANdrive motors of trinamic
+ *
+ * The command code of commands on the control port can be either specified by using the value field
+ * of the 'c'-parameter type or by using the command code directly as parameter type and the value
+ * field of this parameter for the real value.
+ * For controlling by a PC via an ASCII protocol the usage of the 'c'-parameter is recommended, for
+ * internal control commands using the command code as parameter type is recommended.
+ *
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+#include <fpga-log/device/device_PANdrvie.h>
+#include <fpga-log/peripheral_funcs/uart_light_funcs.h>
+#include <stdio.h>
+
+const char* pan_drive_error_codes[6] = PAN_DRIVE_ERROR_CODES;
+
+static void device_PANdrive_send_data(void* const _pan_drive,
+		const unsigned int id, const timestamp_t* const timestamp);
+static void device_PANdrive_new_control_message(void* const _pan_drive,
+		unsigned int count, const control_parameter_t* parameters);
+
+void device_PANdrive_init(device_PANdrive_t* const pan_drive,
+		uart_light_regs_t* const uart_light, const int id) {
+	datastream_source_init(&pan_drive->super, id);  //call parents init function
+	/*
+	 * set method pointer(s) of super-"class" to sub-class function(s)
+	 */
+	pan_drive->super.send_data = device_PANdrive_send_data;
+
+	pan_drive->data_out = &data_port_dummy;
+	pan_drive->status_out = &data_port_dummy;
+
+	pan_drive->control_in = control_port_dummy;
+	pan_drive->control_in.parent = (void*) pan_drive;
+	pan_drive->control_in.new_control_message =
+			device_PANdrive_new_control_message;
+
+	pan_drive->uart_light = uart_light;
+
+	uart_light_enable_rxint(uart_light);
+
+	pan_drive->target_address = PAN_DRIVE_DEFAULT_TARGET_ADDRESS;
+	pan_drive->receive_state = 0;
+	pan_drive->received_status = 0;
+}
+
+control_port_t* device_PANdrive_get_control_in(
+		device_PANdrive_t* const pan_drive) {
+	return &pan_drive->control_in;
+}
+
+void device_PANdrive_set_data_out(device_PANdrive_t* const pan_drive,
+		const data_port_t* const data_in) {
+	pan_drive->data_out = data_in;
+}
+
+void device_PANdrive_set_status_out(device_PANdrive_t* const pan_drive,
+		const data_port_t* const data_in) {
+	pan_drive->status_out = data_in;
+}
+
+static void device_PANdrive_buffer_printf(void* param, const unsigned char byte) {
+	device_PANdrive_t* pan_drive = (device_PANdrive_t*) param;
+	*pan_drive->buf++ = byte;
+}
+
+static void device_PANdrive_send_data(void* const _pan_drive,
+		const unsigned int id, const timestamp_t* const timestamp) {
+	device_PANdrive_t* pan_drive = (device_PANdrive_t*) _pan_drive;
+
+	unsigned char byte;
+	if (uart_light_receive_nb(pan_drive->uart_light, &byte) == UART_OK) {
+		pan_drive->received_value <<= 8;
+		pan_drive->received_value |= cast_to_ulong(byte);
+
+		switch (pan_drive->receive_state) {
+		case 2:
+			pan_drive->received_status = byte;
+			break;
+		case 3: {
+			pan_drive->received_value = 0;
+			pan_drive->reply_command_code = byte;
+
+			char* val_name = "successfully executed";
+			if (pan_drive->received_status <= 6) {
+				val_name = pan_drive_error_codes[pan_drive->received_status - 1];
+			}
+			data_package_t package =
+					{ id, val_name, DATA_TYPE_BYTE, &byte, timestamp };
+			pan_drive->status_out->new_data(pan_drive->status_out->parent, &package);
+			break;
+		}
+		case 7: {
+			unsigned char buffer[32];
+			pan_drive->buf = buffer;
+			stdio_descr.base_adr = pan_drive;
+			stdio_descr.send_byte = device_PANdrive_buffer_printf;
+			printf("command %d", pan_drive->reply_command_code);
+			*pan_drive->buf = 0;
+
+			data_package_t package = { id, buffer, DATA_TYPE_LONG,
+					&pan_drive->received_value, timestamp };
+			pan_drive->data_out->new_data(pan_drive->data_out->parent, &package);
+			break;
+		}
+		default:
+			break;
+		}
+
+		pan_drive->receive_state++;
+		if (pan_drive->receive_state >= 9) {
+			pan_drive->receive_state = 0;
+		}
+	}
+}
+
+static void device_PANdrive_new_control_message(void* const _pan_drive,
+		unsigned int count, const control_parameter_t* parameters) {
+	device_PANdrive_t* pan_drive = (device_PANdrive_t*) _pan_drive;
+
+	pan_drive_command_code_cpt command = 0;
+	unsigned char type = 0;
+	unsigned char mbnum = 0;
+	long value = 0;
+
+	while (count--) {
+		switch (parameters->type) {
+		case PAN_DRIVE_COMMAND:
+			command = parameters->value;
+			break;
+		case PAN_DRIVE_TYPE:
+			type = parameters->value;
+			break;
+		case PAN_DRIVE_MOTOR_NUMBER:
+			mbnum = parameters->value;
+			break;
+		case PAN_DRIVE_BANK_NUMBER:
+			mbnum = parameters->value;
+			break;
+		case PAN_DRIVE_VALUE:
+			value = parameters->value;
+			break;
+		case PAN_DRIVE_MODULE_ADDRESS:
+			pan_drive->target_address = parameters->value;
+			break;
+		default:  //interpret "unknown" parameters as direct command codes
+			command = parameters->type;
+			value = parameters->value;
+			break;
+		}
+		parameters++;
+	}
+	device_PANdrive_send_command_w_mbnum(pan_drive, command, type, mbnum, value);
+}
+
+void device_PANdrive_send_command(device_PANdrive_t* const pan_drive,
+		pan_drive_command_code_cpt command_code, pan_drive_command_type type,
+		long value) {
+	device_PANdrive_send_command_w_mbnum(pan_drive, command_code, type, 0, value);
+}
+
+void device_PANdrive_send_command_w_mbnum(device_PANdrive_t* const pan_drive,
+		pan_drive_command_code_cpt command_code, unsigned char type,
+		unsigned char mbnum, long value) {
+	uart_light_regs_t* uart_light = pan_drive->uart_light;
+
+	unsigned char checksum = pan_drive->target_address;
+	uart_light_send(uart_light, pan_drive->target_address);
+	checksum += command_code;
+	uart_light_send(uart_light, command_code);
+	checksum += type;
+	uart_light_send(uart_light, type);
+	checksum += mbnum;
+	uart_light_send(uart_light, mbnum);
+
+	int i = 4;
+	unsigned char byte;
+	while (i--) {
+		byte = (value >> 24);
+		value <<= 8;
+
+		checksum += byte;
+		uart_light_send(uart_light, byte);
+	}
+	uart_light_send(uart_light, checksum);
+
+	//pan_drive->receive_state = 0;
+	//TODO reset this at some point to react to transmission errors
+	//but here is too early: last reply of PANdrive could still be in transmission
+	//-> use some timeout, e.g. amount of calls of update function? or a real timer?
+}
+
+void device_PANdrive_set_target_address(device_PANdrive_t* const pan_drive,
+		unsigned char new_address) {
+	pan_drive->target_address = new_address;
+}
+
+void device_PANdrive_set_axis_parameter(device_PANdrive_t* const pan_drive,
+		pan_drive_command_type type, long value) {
+	device_PANdrive_send_command(pan_drive, PAN_DRIVE_COMMAND_SET_AXIS_PARAMETER,
+			type, value);
+}
