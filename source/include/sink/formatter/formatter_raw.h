075a474e22336ed534e1fd907261d88f5c3533df
Carsten Bruns <bruns@lichttechnik.tu-darmstadt.de>
2014-12-17 18:46:34 +0100

    new formatter: formatter_raw

---
+++ source/include/sink/formatter/formatter_raw.h
@@ -0,0 +1,27 @@
+/**
+ * @file formatter_raw.h
+ * @brief output log formatter which outputs the received values directly (without additional data, e.g. for GPS messages)
+ * 
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+#ifndef FORMATTER_RAW_H_
+#define FORMATTER_RAW_H_
+
+#include <fpga-log/sink/formatter/formatter.h>
+
+/** 
+ * @brief struct defining a simple formatter which only outputs a message count and the value
+ */
+typedef struct {
+	formatter_t super; 	/**< super-"class": formatter_t */
+} formatter_raw_t;
+
+/**
+ * @brief init function of formatter_raw
+ * 
+ * @param formatter	pointer to the formatter struct
+ */
+void formatter_raw_init(formatter_raw_t* const formatter);
+
+#endif 
---
+++ source/src/formatter_raw.c
@@ -0,0 +1,43 @@
+/**
+ * @file formatter_raw.c
+ * @brief output log formatter which outputs the received values directly (without additional data, e.g. for GPS messages)
+ * 
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+#include <stdio.h>
+#include <fpga-log/sink/formatter/formatter_raw.h>
+#include <fpga-log/long_int.h>
+#include <fpga-log/simple_float.h>
+
+/**
+ * @brief raw formatter output log format function
+ *
+ * @param	formatter	pointer to the formatter struct
+ * @param	package		the incoming package
+ */
+static void formatter_raw_format(void* const formatter,
+		const data_package_t* const package);
+
+void formatter_raw_init(formatter_raw_t* const formatter) {
+	/*
+	 * set method pointer(s) of super-"class" to sub-class function(s)
+	 */
+	formatter->super.format = formatter_raw_format;
+}
+
+static void formatter_raw_format(void* const formatter,
+		const data_package_t* const package) {
+	formatter_t* f = (formatter_t*) formatter;
+	stdio_descr.base_adr = f->param;
+	stdio_descr.send_byte = f->write_byte;
+
+	switch (package->type) {
+	case (DATA_TYPE_BYTE): {
+		f->write_byte(f->param, *((unsigned char* ) package->data));
+		break;
+	}
+	default:
+		printf("unsupported data type in stream!");
+	}
+}
