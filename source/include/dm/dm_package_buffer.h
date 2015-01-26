d6ae40d6b3a0a89a53a1b95ce0721562f43e13f9
Carsten Bruns <bruns@lichttechnik.tu-darmstadt.de>
2015-01-26 18:42:33 +0100

    dm_package_buffer: objects but no module yet

---
+++ source/include/dm/dm_package_buffer.h
@@ -0,0 +1,38 @@
+/**
+ * @file dm_package_buffer.h
+ * @brief module to buffer data_packages until requested
+ *
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+//TODO build a module around this!!!
+#ifndef DM_PACKAGE_BUFFER_H_
+#define DM_PACKAGE_BUFFER_H_
+
+#include <fpga-log/data_port.h>
+
+typedef struct {
+	data_package_t package; /**< the normal package structure */
+
+	unsigned char data[DATA_TYPE_MAX_BYTES]; /**< package data memory */
+	timestamp_t timestamp; /**< timestamp storage */
+} data_package_stored_t;
+
+typedef struct {
+	data_package_stored_t* storage_array; /**< array for storing the actual data */
+	int array_size; /**< size of the storage_arry */
+
+	int next_read; /**< ringbuffer: indicates the position of the next package to read */
+	int next_write; /**< ringbuffer: indicates the position where the next package should be stored */
+	int cur_size; /**< current number of elements in the fifo */
+} data_package_fifo_t;
+
+void _data_package_fifo_init(data_package_fifo_t* data_package_fifo,
+		data_package_stored_t* storage_array, int array_size);
+
+void data_package_fifo_put(data_package_fifo_t* data_package_fifo,
+		data_package_t* package);
+
+data_package_t* data_package_fifo_get(data_package_fifo_t* data_package_fifo);
+
+#endif
---
+++ source/src/dm_package_buffer.c
@@ -0,0 +1,65 @@
+/**
+ * @file dm_package_buffer.c
+ * @brief module to buffer data_packages until requested
+ *
+ * @author Carsten Bruns (bruns@lichttechnik.tu-darmstadt.de)
+ */
+
+#include <fpga-log/dm/dm_package_buffer.h>
+
+void _data_package_fifo_init(data_package_fifo_t* data_package_fifo,
+		data_package_stored_t* storage_array, int array_size) {
+	data_package_fifo->storage_array = storage_array;
+	data_package_fifo->array_size = array_size;
+
+	data_package_fifo->next_read = 0;
+	data_package_fifo->next_write = 0;
+	data_package_fifo->cur_size = 0;
+}
+
+static void data_package_wrap_pointers(data_package_fifo_t* data_package_fifo) {
+	if (data_package_fifo->next_write >= data_package_fifo->array_size)
+		data_package_fifo->next_write -= data_package_fifo->array_size;
+	if (data_package_fifo->next_read >= data_package_fifo->array_size)
+		data_package_fifo->next_read -= data_package_fifo->array_size;
+}
+
+void data_package_fifo_put(data_package_fifo_t* data_package_fifo,
+		data_package_t* package) {
+	data_package_stored_t* element = data_package_fifo->storage_array
+			+ data_package_fifo->next_write;  //get element to write
+
+	//store package with data and re-arrange pointers in package to our copy
+	element->package = *package;
+	int i;
+	for (i = 0; i < data_type_byte_sizes[package->type]; i++) {  //copy data from package
+		element->data[i] = *((unsigned char*) (package->data + i));
+	}
+	element->package.data = &element->data;
+	element->timestamp = *package->timestamp;
+	element->package.timestamp = &element->timestamp;
+
+	data_package_fifo->next_write++;  //increase write pointer
+
+	if (data_package_fifo->cur_size < data_package_fifo->array_size) {
+		data_package_fifo->cur_size++;  //increase current fifo size
+	} else {
+		data_package_fifo->next_read++;  //fifo full, data loss, move read pointer to new first element
+	}
+
+	data_package_wrap_pointers(data_package_fifo);  //re-adjust the pointers (wrap-around, ring-buffer)
+}
+
+data_package_t* data_package_fifo_get(data_package_fifo_t* data_package_fifo) {
+	if (data_package_fifo->cur_size == 0)
+		return 0;
+
+	data_package_stored_t* element = data_package_fifo->storage_array
+			+ data_package_fifo->next_read;  //get element to read
+
+	data_package_fifo->cur_size--;
+	data_package_fifo->next_read++;
+	data_package_wrap_pointers(data_package_fifo);  //re-adjust the pointers (wrap-around, ring-buffer)
+
+	return &element->package;
+}
