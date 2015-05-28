8930c38684d8a0bdf991ded62142bdd85553cc31
Carsten Bruns <bruns@lichttechnik.tu-darmstadt.de>
2015-05-28 22:09:45 +0200

    config-tool: show method descriptions in method add dialog

--- config-tool/gui/newmethoddialog.cpp
+++ config-tool/gui/newmethoddialog.cpp
@@ -12,7 +12,8 @@ NewMethodDialog::NewMethodDialog(std::list<CMethod *> methods, QWidget *parent) 
     ui->setupUi(this);
 
     QStandardItemModel* model = new QStandardItemModel();
-    model->setHorizontalHeaderItem( 0, new QStandardItem(QString::fromUtf8("verfügbare Methoden:")));
+    model->setHorizontalHeaderItem( 0, new QStandardItem(QString::fromUtf8("Methodenname")));
+    model->setHorizontalHeaderItem( 1, new QStandardItem(QString::fromUtf8("Beschreibung")));
 
     int pos = 0;
     for (list<CMethod*>::iterator i = methods.begin(); i != methods.end(); i++) {
@@ -20,14 +21,20 @@ NewMethodDialog::NewMethodDialog(std::list<CMethod *> methods, QWidget *parent) 
             QStandardItem* item = new QStandardItem((*i)->getName().c_str());
             item->setEditable(false);
             item->setSelectable(true);
-            item->setData((*i)->getDescription().c_str(), Qt::ToolTipRole);
             model->setItem(pos, item);
+
+            QStandardItem* item2 = new QStandardItem((*i)->getDescription().c_str());
+            item2->setEditable(false);
+            item2->setSelectable(true);
+            model->setItem(pos, 1, item2);
 
             pos++;
         }
     }
 
     ui->methodList->setModel(model);
+    ui->methodList->resizeColumnToContents(0);
+    ui->methodList->resizeColumnToContents(1);
 }
 
 string NewMethodDialog::getSelectedMethod() {
